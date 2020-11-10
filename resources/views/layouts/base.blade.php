1989b320e2b8c9dac8b7d4201f1acf950d0b6deb
Anthony Rappa <rappa819@gmail.com>
2020-11-10 09:56:29 -0500

    Remove packages

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,18 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ appName() }} | @yield('title')</title>
     <meta name="description" content="@yield('meta_description', appName())">
-    <meta name="author" content="@yield('meta_author', 'Anthony Rappa')"> {{-- TODO: Change me --}}
+    <meta name="author" content="@yield('meta_author', 'Anthony Rappa')">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @yield('meta')
 
     @stack('before-styles')
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     <livewire:styles />
     @stack('after-styles')
 
     @include('includes.partials.ga')
 </head>
-<body class="@yield('classes[body]')">
+<body>
     @yield('body')
 
     @stack('before-scripts')
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,9 @@
 const mix = require('laravel-mix');
-require("laravel-mix-tailwind");
 
-mix.js("resources/js/app.js", "public/js/app.js")
-    .sass("resources/sass/app.scss", "public/css/app.css")
-    .tailwind("./tailwind.config.js")
+mix.js("resources/js/app.js", "public/js")
+    .postCss('resources/css/app.css', 'public/css', [
+        require('tailwindcss'),
+    ])
     .extract([
         'alpinejs',
         'jquery',
